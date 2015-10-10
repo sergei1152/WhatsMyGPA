57cb9718b76a60d1d1c77200de23b96aba3ae0be
Serge Babayan <serj.b.babayan@gmail.com>
2015-10-10 14:56:47 -0400

    Updated gulp configuration for maintainability

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,32 @@ var jshint = require('gulp-jshint');
 var jshint_stylish = require('jshint-stylish');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-minify-css');
+var runSequence = require('run-sequence');
 
-var javascript_libraries=[ './scripts/lib/angular/angular.min.js', 'scripts/lib/jquery/dist/jquery.min.js','./scripts/lib/selectize/dist/js/standalone/selectize.min.js','./scripts/lib/ui-select/dist/select.min.js',
-    'scripts/lib/angular-sanitize/angular-sanitize.min.js'];
-javascript_libraries.push('./scripts/*.js');
-var javascript_files=javascript_libraries;
+//all the scripts the app uses
+var scripts=[
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/angular/angular.min.js',
+    'bower_components/angular-sanitize/angular-sanitize.min.js',
+    'bower_components/ui-select/dist/select.min.js',
+    'scripts/*.js'
+    ];
+
+//all the css files the app uses
+var styles=[
+    'bower_components/ui-select/dist/select.min.css',
+    'style/dist/*.css',
+    '!style/dist/dist.css' //not the actual dist file
+    ];
 
 gulp.task('default', function() {
-    console.log("NOTE: Please use either gulp build or gulp watch");
-});
+     runSequence('build', 'watch');
+ });
 
 //for production builds
 gulp.task('build', function() {
-    gulp.src(javascript_files)
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./scripts/dist/'));
-
-    gulp.src('./style/**.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./style/dist/'));
+    runSequence('js-hint','js-concat','js-uglify','sass','css-concat','css-min');
 });
 
 //so that livereload works with html
@@ -49,18 +49,24 @@ gulp.task('js-hint', function() {
 
 //concatanates all the javascript files
 gulp.task('js-concat', function() {
-    gulp.src(javascript_files)
+    gulp.src(scripts)
         .pipe(concat('main.js'))
         .pipe(gulp.dest('./scripts/dist/'))
         .pipe(livereload());
+});
+
+//minifies the main.js dist file
+gulp.task('js-uglify', function() {
+    gulp.src('scripts/dist/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./scripts/dist/'));
 });
 
 //compiles, minifies all the sass files
 gulp.task('sass', function() {
     gulp.src('./style/**.scss')
         .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
+            }).on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -69,9 +75,24 @@ gulp.task('sass', function() {
         .pipe(livereload());
 });
 
+//concatanates all the css files into a single file
+gulp.task('css-concat', function() {
+     gulp.src(styles)
+        .pipe(concat('dist.css'))
+        .pipe(gulp.dest('style/dist/'))
+        .pipe(livereload());
+});
+
+//minifies the main css file
+gulp.task('css-min', function() {
+     gulp.src(['style/dist/dist.css'])
+        .pipe(cssmin({compatibility: 'ie8'}))
+        .pipe(gulp.dest('style/dist/'));
+});
+
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch('./style/*.scss', ['sass']);
+    gulp.watch('./style/*.scss', ['sass','css-concat']);
     gulp.watch('./*.html', ['html-live']);
     gulp.watch('./scripts/*.js', ['js-hint','js-concat']);
 });
