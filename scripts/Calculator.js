6f8609ce80138c1cce2d5b72909ecaecbe738f81
Serge Babayan <serj.b.babayan@gmail.com>
2015-10-12 14:17:05 -0400

    Refactoring

---
+++ scripts/Calculator.js
@@ -0,0 +1,9 @@
+angular.module('Calculator',['ReportCard'])
+
+.factory('Calculate',["ReportCard", function(ReportCard){
+	var Calculate=function(university){
+
+		return results;
+	};
+	return Calculate;
+}]);
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,19 @@
-angular.module('WhatsMyGPA.ca', ['Universities','ReportCard', 'ngSanitize','ui.select'])
+angular.module('WhatsMyGPA.ca', ['Universities','ReportCard','Calculator', 'ngSanitize','ui.select'])
 
-.controller('InputController',['$scope','UniversityList','ReportCard',function($scope,UniversityList,ReportCard){
+.controller('InputController',['$scope','UniversityList','ReportCard','Calculate',function($scope,UniversityList,ReportCard,Calculate){
+
 	$scope.UniversityList=UniversityList;
-  $scope.selectedUniversity={}; //contains the "value" of the univeristy
   $scope.ReportCard=ReportCard;
-  $scope.ReportCard.addSemester();
-  $scope.selectedGradeInputType={};
-  $scope.print=function(){
-    console.log(ReportCard.semesters[0]);
-    console.log($scope.selectedGradeInputType);
-  }
-  //called when a user selects their university/college
-  $scope.universitySelected=function(selectedUniversity){
-    $scope.selectedUniversity=selectedUniversity;
-    console.log(selectedUniversity); //same object as selectedUniversity
+
+  $scope.university={
+    selected: null, //the selected university
+    selectedGradeConversion: null, //the input grade type the user selected
+    results: null //the results of the calculation
   };
+  
+  $scope.Calculate=function(){
+    $scope.university.results=Calculate($scope.university);
+  }
+  
+  $scope.ReportCard.addSemester(); //initializes the report card with a semester for the user to fill in
 }]);
