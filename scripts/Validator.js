e171b05de72acd18a1776cc3d0256bc6c589faf3
Serge Babayan <serj.b.babayan@gmail.com>
2015-10-25 16:50:58 -0400

    Created validation module

---
+++ scripts/Validator.js
@@ -0,0 +1,35 @@
+angular.module('Validator',[])
+
+//Regex used throughout the calculator
+.factory('Regex', function() {
+    return {
+        letterGradeRegex: /^[a-zA-Z-+]{1,3}$/,
+        numberGradeRegex: /^[0-9.]{1,5}$/,
+        creditWeightRegex: /^[0-9.]{1,4}$/
+    };
+})
+
+.factory('Validator',['Regex',function(Regex){
+	//checks validity of a grade using the predefined regex
+    var isValid = function(value, selectedGradeConversion) {
+        if (selectedGradeConversion.type === 'letter') {
+            if (value.match(Regex.letterGradeRegex)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (selectedGradeConversion.type === 'number') {
+            if (value.match(Regex.numberGradeRegex)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            console.error('Wrong type property for selectedGradeConversion');
+        }
+    };
+
+    return {
+    	isValid: isValid
+    };
+}]);
