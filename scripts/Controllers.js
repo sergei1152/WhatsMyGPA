85f7c57c70e780e7a7bde639f900c4a42ee04109
Serge Babayan <serj.b.babayan@gmail.com>
2015-10-25 17:58:15 -0400

    Renamed main to Controllers

---
+++ scripts/Controllers.js
@@ -0,0 +1,35 @@
+angular.module('WhatsMyGPA.ca', ['Universities','ReportCard','Calculator', 'ngSanitize','ui.select','Facebook','Results'])
+
+.controller('InputController',['$scope','UniversityList','ReportCard','Calculate','setUpResults','Results',function($scope,UniversityList,ReportCard,Calculate,setUpResults,Results){
+	$scope.UniversityList=UniversityList;
+  $scope.ReportCard=ReportCard;
+  $scope.university={
+    selected: null, //the selected university
+    selectedGradeInput: null //the input grade type the user selected
+  };
+
+  $scope.Calculate=function(){
+    Calculate($scope.university);
+  };
+
+  $scope.universitySelected=function(){
+    setUpResults($scope.university.selected.value);
+  };
+
+  $scope.ReportCard.addSemester(); //initializes the report card with a semester for the user to fill in
+}])
+
+.controller('OutputController', ['$scope', 'Results','Facebook', function($scope, Results,Facebook){
+  $scope.Results=Results;
+
+  $scope.isEmpty=function(object){ //so that if the results are empty, 
+    for(var key in object){
+      if(object.hasOwnProperty(key)){
+        return false;
+      }
+    }
+    return true;
+  };
+
+  $scope.Facebook=Facebook;
+}]);
