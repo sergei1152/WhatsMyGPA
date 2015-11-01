73298081dab9ea616119701e689407a41584e8ad
Serge Babayan <serj.b.babayan@gmail.com>
2015-11-01 10:00:40 -0500

    added in edge case for when user re-selects university

--- scripts/Controllers.js
+++ scripts/Controllers.js
@@ -35,6 +35,16 @@ angular.module('WhatsMyGPA.ca', ['Universities','ReportCard','Calculator', 'ngSa
     
   $scope.universitySelected=function(){
     setUpResults($scope.university.selected.value);
+    if($scope.university.selectedGradeInput){ //if a grade input was already selected and the other university has the grade input type
+      var selectedGradeConversion=$scope.university.selected.value.gradeConversions[$scope.university.selectedGradeInput];
+      if(selectedGradeConversion){
+        Validator.buildLetterIndex(selectedGradeConversion);
+      }
+      else{ //if the grade input type doesnt exist in the university
+        
+
+      }
+    }
   };
 
   $scope.gradeInputTypeSelected=function(){
