6f1a128339878e9968f5075a75ce49ce0c77cbf1
Serge Babayan <serj.b.babayan@gmail.com>
2015-11-04 21:13:57 -0500

    Added laurier university

--- scripts/Universities.js
+++ scripts/Universities.js
@@ -396,6 +396,11 @@ angular.module('Universities', [])
                 }
             }
         },
+        /*Based from:
+            http://www.ryerson.ca/studentguide/academics/grades--gpa--academic-standing.html
+            and
+            http://studentsuccess.mcmaster.ca/students/tools/gpa-conversion-chart.html
+        */
         ryersonuniversity: {
             name: 'Ryerson University',
             location: {
@@ -470,7 +475,188 @@ angular.module('Universities', [])
                     }]
                 }
             }
-        }
-        //uni
+        },
+        /*Retrieved from http://legacy.wlu.ca/calendars/section.php?cal=1&s=505&sp=1723&ss=2151&y=53
+        and http://legacy.wlu.ca/page.php?grp_id=1402&p=12614
+        */
+         wilfridlaurieruniversity: {
+            name: 'Wilfred Laurier University',
+            location: {
+                city: "Waterloo",
+                state: "Ontario",
+                country: "Canada"
+            },
+            commonCreditWeights: [0,0.25,0.5,1],
+            gradeConversions: {
+                gpa: gpa,
+                "percentage": {
+                    name: "Percentage",
+                    example: "75%, 67%",
+                    type: "number",
+                    min: 0,
+                    max: 100,
+                    gpaConversion: [{
+                        value: 4,
+                        min: 90,
+                        max: 100
+                    }, {
+                        value: 3.9,
+                        min: 85,
+                        max: 89
+                    },{
+                        value: 3.7,
+                        min: 80,
+                        max: 84
+                    }, {
+                        value: 3.3,
+                        min: 77,
+                        max: 79
+                    }, {
+                        value: 3,
+                        min: 73,
+                        max: 76
+                    }, {
+                        value: 2.7,
+                        min: 70,
+                        max: 72
+                    }, {
+                        value: 2.3,
+                        min: 67,
+                        max: 69
+                    }, {
+                        value: 2,
+                        min: 63,
+                        max: 66
+                    }, {
+                        value: 1.7,
+                        min: 60,
+                        max: 62
+                    }, {
+                        value: 1.3,
+                        min: 57,
+                        max: 59
+                    }, {
+                        value: 1,
+                        min: 53,
+                        max: 56
+                    }, {
+                        value: 0.7,
+                        min: 50,
+                        max: 52
+                    }, {
+                        value: 0,
+                        min: 0,
+                        max: 49
+                    }]
+                }, 
+                "letter":{
+                    name: "Letter",
+                    example:"B, A+",
+                    type: "letter",
+                    gpaConversion: [{
+                        value: 4,
+                        letters: ['A+']
+                    }, {
+                        value: 3.9,
+                        letters: ['A']
+                    },{
+                        value: 3.7,
+                        letters: ['A-']
+                    }, {
+                        value: 3.3,
+                        letters: ['B+']
+                    }, {
+                        value: 3,
+                        letters: ['B']
+                    }, {
+                        value: 2.7,
+                        letters: ['B-']
+                    }, {
+                        value: 2.3,
+                        letters: ['C+']
+                    }, {
+                        value: 2,
+                        letters: ['C']
+                    }, {
+                        value: 1.7,
+                        letters: ['C-']
+                    }, {
+                        value: 1.3,
+                        letters: ['D+']
+                    }, {
+                        value: 1,
+                        letters: ['D']
+                    }, {
+                        value: 0.7,
+                        letters: ['D-']
+                    }, {
+                        value: 0,
+                        letters: ['F','E', 'XF','DR']
+                    }]
+                },
+                "point12":{
+                    name:"12-Point",
+                    example:"6, 11.5",
+                    min: 0,
+                    max: 12,
+                    type: "number",
+                    gpaConversion:[
+                        {
+                            value: 4,
+                            min: 12,
+                            max: 12
+                        },{
+                            value: 3.9,
+                            min: 11, 
+                            max: 11
+                        },{
+                            value: 3.7,
+                            min: 10, 
+                            max: 10
+                        },{
+                            value: 3.3,
+                            min: 9,
+                            max: 9
+                        },{
+                            value: 3,
+                            min: 8,
+                            max: 8
+                        },{
+                            value: 2.7,
+                            min: 7,
+                            max: 7
+                        },{
+                            value: 2.3,
+                            min: 6,
+                            max: 6
+                        },{
+                            value: 2,
+                            min: 5,
+                            max: 5
+                        },{
+                            value: 1.7,
+                            min: 4, 
+                            max: 4
+                        },{
+                            value: 1.3,
+                            min: 3,
+                            max: 3
+                        },{
+                            value: 1,
+                            min: 2, 
+                            max: 2
+                        },{
+                            value: 0.7,
+                            min: 1, 
+                            max: 1
+                        },{
+                            value: 0,
+                            min: 0,
+                            max: 0
+                        }
+                    ]
+                }
+            }
+        },
     };
 });
