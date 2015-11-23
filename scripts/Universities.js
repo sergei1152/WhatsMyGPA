90a57e9efdd6712d64079f06402933e3201e2de1
Serge Babayan <serj.b.babayan@gmail.com>
2015-11-22 22:47:50 -0500

    Added windsor

--- scripts/Universities.js
+++ scripts/Universities.js
@@ -1811,6 +1811,195 @@ and http://legacy.wlu.ca/page.php?grp_id=1402&p=12614
           }]
         }
       }
+    },
+    windsoruniversity:{
+      name: "University of Windsor",
+      location:{
+        city:"Windsor",
+        state:"Ontario",
+        country:"CA"
+      },
+      commonCreditWeights: [1,2.5,3,4.25,6,8],
+      sources:['http://www1.uwindsor.ca/advisingcentre/marking-scale','http://studentsuccess.mcmaster.ca/students/tools/gpa-conversion-chart.html'],
+      gradeConversions: {
+        gpa: gpa,
+        percentage: {
+          name: "Percentage",
+          example: "75%, 67%",
+          type: "number",
+          min: 0,
+          max: 100,
+          gpaConversion: [{
+            value: 4,
+            min: 93,
+            max: 100
+          },{
+            value: 3.9,
+            min: 86,
+            max: 92
+          }, {
+            value: 3.7,
+            min: 80,
+            max: 85
+          }, {
+            value: 3.3,
+            min: 77,
+            max: 79
+          }, {
+            value: 3,
+            min: 73,
+            max: 76
+          }, {
+            value: 2.7,
+            min: 70,
+            max: 72
+          }, {
+            value: 2.3,
+            min: 67,
+            max: 69
+          }, {
+            value: 2,
+            min: 63,
+            max: 66
+          }, {
+            value: 1.7,
+            min: 60,
+            max: 62
+          },{
+            value: 1.3,
+            min: 57,
+            max: 59
+          },{
+            value: 1,
+            min: 53,
+            max: 56
+          },{
+            value: 0.7,
+            min: 50,
+            max: 52
+          },{
+            value: 0,
+            min: 35,
+            max: 49
+          },{
+            value: 0,
+            min: 0,
+            max: 34
+          }]
+        },
+        letter: { 
+          name: "Letter",
+          example: "B, A+",
+          type: "letter",
+          gpaConversion: [{
+            value: 4,
+            letters: ['A+']
+          }, {
+            value: 3.9,
+            letters: ['A']
+          }, {
+            value: 3.7,
+            letters: ['A-']
+          },{
+            value: 3.3,
+            letters: ['B+']
+          }, {
+            value: 3,
+            letters: ['B']
+          }, {
+            value: 2.7,
+            letters: ['B-']
+          },{
+            value: 2.3,
+            letters: ['C+']
+          }, {
+            value: 2,
+            letters: ['C']
+          },{
+            value: 1.7,
+            letters: ['C-']
+          },{
+            value: 1.3,
+            letters: ['D+']
+          }, {
+            value: 1,
+            letters: ['D']
+          }, {
+            value: 0.7,
+            letters: ['D-']
+          },{
+            value: 0,
+            letters: ['F']
+          },{
+            value: 0,
+            letters: ['F-']
+          }]
+        },
+        point13: {
+          name: "13 Point",
+          example: "13, 9.5",
+          type: "number",
+          min: 0,
+          max: 13,
+          gpaConversion: [{
+            value: 4,
+            min: 13,
+            max: 13
+          },{
+            value: 3.9,
+            min: 12,
+            max: 12
+          }, {
+            value: 3.7,
+            min: 11,
+            max: 11
+          }, {
+            value: 3.3,
+            min: 10,
+            max: 10
+          }, {
+            value: 3,
+            min: 9,
+            max: 9
+          }, {
+            value: 2.7,
+            min: 8,
+            max: 8
+          }, {
+            value: 2.3,
+            min: 7,
+            max: 7
+          }, {
+            value: 2,
+            min: 6,
+            max: 6
+          }, {
+            value: 1.7,
+            min: 5,
+            max: 5
+          },{
+            value: 1.3,
+            min: 4,
+            max: 4
+          },{
+            value: 1,
+            min: 3,
+            max: 3
+          },{
+            value: 0.7,
+            min: 2,
+            max: 2
+          },{
+            value: 0,
+            min: 1,
+            max: 1
+          },{
+            value: 0,
+            min: 0,
+            max: 0
+          }]
+        }
+      }
     }
   };
 });
