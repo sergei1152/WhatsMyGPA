cf92b57258ec85563e5b8032a4b1c2b4baf17146
Serge Babayan <serj.b.babayan@gmail.com>
2015-11-28 11:42:01 -0500

    Implemented a better key based search

--- scripts/Filters.js
+++ scripts/Filters.js
@@ -1,37 +1,42 @@
 angular.module('Filters',[])
 
-.filter('uniFilter', ['$filter',function($filter) { //the custom university list filter. Names take priority over location
-  return function(items, props) {
-    var out = [];
-    if (angular.isArray(items)) {
-      items.forEach(function(item) {
-        var university=item.value;
-        var keys = Object.keys(props);
-        itemMatches = false;
-        for (var i = 0; i < keys.length; i++) {
-          var key = keys[i];
-          var text = props[key].toLowerCase();
-          if(university[key]){ //if university key
-            if(university[key].toString().toLowerCase().indexOf(text) !== -1) {
-              itemMatches = true;
-              break;
-            }
+.filter('uniFilter', ['$filter',function($filter) { //the custom university list filter. Names take priority over key over location
+  return function(items, props) { //items is the list of universities as an object, props is the search string
+    var key_out=[]; //list of universities that match key
+    var name_out = []; //list of universities that match name
+    var location_out=[]; //list of universities that match location
+    var out=[]; //concatanated list of name over location
+    var search=props.toLowerCase();
+    if (typeof items==='object' && search!=='') { //if not an empty search string
+      for(var university in items){
+        if(items.hasOwnProperty(university)){
+          if(items[university].name.toLowerCase().indexOf(props)!==-1){ //match on the name
+            name_out.push(items[university]);
           }
-          else if(university.location[key]){ //if location key
-            if(university.location[key].toString().toLowerCase().indexOf(text) !== -1) {
-              itemMatches = true;
-              break;
-            }
+          //if it matches location
+          else if (items[university].location.city.toLowerCase().indexOf(search)!==-1 || items[university].location.country.toLowerCase().indexOf(search)!==-1 || items[university].location.state.toLowerCase().indexOf(search)!==-1){
+            location_out.push(items[university]);
+          }
+          else if (university.toLowerCase().indexOf(search)!==-1){ //key matches
+            key_out.push(items[university]);
           }
         }
-        if (itemMatches) {
-          out.push(item);
+      }
+      name_out=$filter('orderBy')(name_out, 'name', false); //filter the resulting array in alphabetical order
+      location_out=$filter('orderBy')(location_out, 'name', false); //filter the resulting array in alphabetical order
+      key_out=$filter('orderBy')(key_out, 'name', false); //filter the resulting array in alphabetical order
+      out=name_out.concat(location_out.concat(key_out));
+    } 
+    else if(typeof items ==='object') { //just return the entire list of universities in alphabetical order
+      for(var uni in items){
+        if(items.hasOwnProperty(uni)){
+          out.push(items[uni]);
         }
-      });
-      out=$filter('orderBy')(out, 'value.name', false); //filter the resulting array in alphabetical order
-    } else {
-      // Let the output be the input untouched
-      out = items;
+      }
+      out=$filter('orderBy')(out, 'name', false); //filter the resulting array in alphabetical order
+    }
+    else{
+      console.error('WARNING: uniFilter only works with objects as inputs');
     }
     return out;
   };
