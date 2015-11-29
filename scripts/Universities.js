ee570a290c3c81d027c8c37ad7380cd0bcdfc0cf
Serge Babayan <serj.b.babayan@gmail.com>
2015-11-29 02:03:57 -0500

    Added george brown and humber college

--- scripts/Universities.js
+++ scripts/Universities.js
@@ -3127,6 +3127,123 @@ and http://legacy.wlu.ca/page.php?grp_id=1402&p=12614
           }]
         }
       }
-    }
+    },
+    georgebrowncollege:{
+      name: "George Brown College",
+      location:{
+        city:"Toronto",
+        state:"Ontario",
+        country:"CA"
+      },
+      commonCreditWeights: [0,1],
+      sources:['http://www.georgebrown.ca/current_students/grades/'],
+      gradeConversions: {
+        gpa: gpa,
+        letter: {
+          name: "Letter",
+          example: "B, A+",
+          type: "letter",
+          gpaConversion: [{
+            value: 4,
+            letters: ['A+','A']
+          }, {
+            value: 3.7,
+            letters: ['A-']
+          },  {
+            value: 3.3,
+            letters: ['B+']
+          }, {
+            value: 3,
+            letters: ['B']
+          }, {
+            value: 2.7,
+            letters: ['B-']
+          }, {
+            value: 2.3,
+            letters: ['C+']
+          }, {
+            value: 2,
+            letters: ['C']
+          }, {
+            value: 1.7,
+            letters: ['C-']
+          }, {
+            value: 1.3,
+            letters: ['D+']
+          },{
+            value: 1,
+            letters: ['D']
+          }, {
+            value: 0,
+            letters: ['F']
+          }]
+        },
+        percentage:{
+          name: "Percentage",
+          example: "75%, 67%",
+          type: "number",
+          min: 0,
+          max: 100,
+          gpaConversion: [{
+            value: 4,
+            min: 86,
+            max: 100
+          }, {
+            value: 3.7,
+            min: 80,
+            max: 85
+          }, {
+            value: 3.3,
+            min: 77,
+            max: 79
+          }, {
+            value: 3,
+            min: 73,
+            max: 76
+          }, {
+            value: 2.7,
+            min: 70,
+            max: 72
+          }, {
+            value: 2.3,
+            min: 67,
+            max: 69
+          }, {
+            value: 2,
+            min: 63,
+            max: 66
+          }, {
+            value: 1.7,
+            min: 60,
+            max: 62
+          },{
+            value: 1.3,
+            min: 57,
+            max: 59
+          },{
+            value: 1,
+            min: 50,
+            max: 56
+          },{
+            value: 0,
+            min: 0,
+            max: 49
+          }]
+        }
+      }
+    },
+    humbercollege:{
+      name: "Humber College",
+      location:{
+        city:"Toronto",
+        state:"Ontario",
+        country:"CA"
+      },
+      commonCreditWeights: [0,1,3,6],
+      sources:[],
+      gradeConversions: {
+        percentage:commonPercentage1
+      }
+    },
   };
 });
